c43cae09582e928d13529be7167837d55b943967
tim-peterson <petersontimr@gmail.com>
2018-09-10 09:36:17 -0500

    adding dna damage and individual sql queries, get rid of LIKE2

--- figure4.sql
+++ figure4.sql
@@ -15,7 +15,7 @@ join
 (select  c.symbol as gene, c.logFC as c_fold, c.`AveExpr` as c_avg, c.`P.Value` as c_pval from tbone.cheng_cancer_her2_1000vs2500days c where c.symbol !='.' and c.`P.Value` <= 0.05 group by symbol) c
 on o.gene=c.gene
 group by o.gene
-
+ 
 
 /* 165 genes CRISPRi-AFF intersection */
 select * from (select * from aff_multiple_case_only_variants_v5) AFF
@@ -45,14 +45,14 @@ on AFF.gene=RNA.gene
 group by AFF.gene
 
 /* 5 genes ONJ/DTC gene expression-AFF-CRISPRi intersection */
-select * from (select o.*, c_fold, c_avg, c_pval from 
+select * from (select * from (select o.*, c_fold, c_avg, c_pval from 
 (select o.symbol as gene, o.logFC as o_fold, o.`AveExpr` as o_avg, o.`P.Value` as o_pval from tbone.cheng_onj o where o.symbol !='.' and o.`P.Value` <= 0.05 group by symbol) o
 join
 (select  c.symbol as gene, c.logFC as c_fold, c.`AveExpr` as c_avg, c.`P.Value` as c_pval from tbone.cheng_cancer_her2_1000vs2500days c where c.symbol !='.' and c.`P.Value` <= 0.05 group by symbol) c
 on o.gene=c.gene
 group by o.gene) RNA
 join
-(select a.gene, a.`r avg3 Mann-Whitney p-value` as crispri_aln_pval, a.`r avg3 average phenotype of strongest 3` as crispri_aln_rho,
+(select a.gene as crispri_gene, a.`r avg3 Mann-Whitney p-value` as crispri_aln_pval, a.`r avg3 average phenotype of strongest 3` as crispri_aln_rho,
 z.`r avg3 Mann-Whitney p-value` as crispri_zol_pval, z.`r avg3 average phenotype of strongest 3` as crispri_zol_rho  
 from crispri.`aln` a
 join 
@@ -60,8 +60,10 @@ crispri.`zol_digested_copy` z
 on a.gene=z.gene
 where 
 (abs(a.`r avg3 average phenotype of strongest 3`) > 0.1 or abs(z.`r avg3 average phenotype of strongest 3`) > 0.1) and (a.`r avg3 Mann-Whitney p-value` <= 0.05 or z.`r avg3 Mann-Whitney p-value` <= 0.05) and a.gene not like "%pseudo_%" and z.gene not like "%pseudo_%") CRISPRI
-on RNA.gene=CRISPRI.gene
+on RNA.gene=CRISPRI.crispri_gene
 join
-(select * from aff_multiple_case_only_variants_v5) AFF
-on AFF.gene=RNA.gene
-group by RNA.gene
+(select gene as aff_gene, patient1,patient2,patient3,patient4 from aff_multiple_case_only_variants_v5) AFF
+on AFF.aff_gene=RNA.gene
+group by RNA.gene) a
+join aff_all_variants_v3_copy b
+on b.gene like concat( '%', a.gene, '%')  
