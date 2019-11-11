92adaea79c8ec6d81a636fee6816d5da49c9fe4d
Tim Peterson <petersontimr@gmail.com>
2019-11-11 11:15:16 -0600

    updating figure 4a/b with revision queries 3

--- figure4.sql
+++ figure4.sql
@@ -103,12 +103,13 @@ on crispria.gene = WES.genec
 
 /* 3 genes ONJ/DTC gene expression-WES-CRISPRi/a intersection */
 
-select * from (select * from (select o.*, c_fold, c_avg, c_pval from 
+select * from (select o.*, c_fold, c_avg, c_pval from 
 (select o.symbol as gene, o.logFC as o_fold, o.`AveExpr` as o_avg, o.`P.Value` as o_pval from tbone.cheng_onj o where o.symbol !='.' and o.`P.Value` <= 0.05 group by symbol) o
 join
 (select  c.symbol as gene, c.logFC as c_fold, c.`AveExpr` as c_avg, c.`P.Value` as c_pval from tbone.cheng_cancer_her2_1000vs2500days c where c.symbol !='.' and c.`P.Value` <= 0.05 group by symbol) c
 on o.gene=c.gene
-group by o.gene) RNA
+group by o.gene
+) RNA
 join
 (select a.gene as crispri_gene, (abs(a.`r avg3 average phenotype of strongest 3`) + abs(z.`r avg3 average phenotype of strongest 3`) + abs(a2.`r avg3 average phenotype of strongest 3`) + abs(z2.`r avg3 average phenotype of strongest 3`)) sumall, a.`r avg3 Mann-Whitney p-value` as crispri_aln_pval, a.`r avg3 average phenotype of strongest 3` as crispri_aln_rho,
 z.`r avg3 Mann-Whitney p-value` as crispri_zol_pval, z.`r avg3 average phenotype of strongest 3` as crispri_zol_rho,
@@ -128,13 +129,16 @@ where
 ) CRISPRI
 on RNA.gene=CRISPRI.crispri_gene
 join
-(select gene as aff_gene, patient1,patient2,patient3,patient4 from aff_multiple_case_only_variants_v5) AFF
-on AFF.aff_gene=RNA.gene
-group by RNA.gene) a
-join aff_all_variants_v3 b
-on b.gene like concat( '%', a.gene, '%') 
+(select a.gene as WES_gene from (select * from Annotated_ONJ_WES20 where carriercount > 0 and (RefGeneFunction_ExonicFunction like "%nonsy%" or RefGeneFunction_ExonicFunction like "%,frameshift_deletion%") group by gene) a
+join 
+aff_multiple_case_only_variants_v5 b
+on
+a.`gene`=b.gene) WES
+on RNA.gene=WES.WES_gene
 
 
+
+ 
 /* first draft queries */
 
 /* select all 2357 genes from ALN or ZOL CRISPRi screen with absolute value rho phenotype > 0.1 and p value <= 0.05  */
